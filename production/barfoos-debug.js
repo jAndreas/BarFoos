9cf010b5487bcfccf92dee1a154675f9265a40fa
Andreas <jandy@typeofnan.com>
2011-12-05 03:18:39 +0100

    rebuild production files

--- production/barfoos-debug.js
+++ production/barfoos-debug.js
@@ -10599,15 +10599,7 @@ $.transform = {
 		Public.trim = function _trim() {
 			return $.trim.apply( null, arguments );
 		};
-		
-		Public.Promise = function _Promise() {
-			return $.Deferred.apply( null, arguments );
-		};
-		
-		Public.when	= function _when() {
-			return $.when.apply( null, arguments );
-		};
-		
+				
 		Public.extend = function _extend() {
 			return $.extend.apply( null, arguments );
 		};
@@ -11949,5 +11941,33 @@ $.transform = {
 				}
 			});
 		}
+	});
+}());/* 
+ * core_plugin_promises.js
+ * ------------------------------
+ * Core plugin: This plugin implements the BarFoos promise maker
+ * This is a clone from jQuerys Deferred objects written by Julian Aubourg, with some slight modifications
+ * 
+ * This code runs in strict mode (if supported by the environment).
+ * ------------------------------
+ * Author: Andreas Goebel
+ * Date: 2011-12-05
+ * Changed: 2011-12-05 - created
+ */
+
+!(function _core_plugin_promises_wrap() {
+	"use strict";
+	
+	Object.lookup( 'BarFoos.Core.plugin', 0 ).execute(function( win, doc, $, Private, Public, Sandbox, App, undef ) {
+		/****** BASE LIBRARY ABSTRACTIONS ## Zepto *************** *******/
+		/****** ************************************************** *******/
+		
+		Public.Promise = function _Promise() {
+			return $.Deferred.apply( null, arguments );
+		};
+		
+		Public.when	= function _when() {
+			return $.when.apply( null, arguments );
+		};
 	});
 }());
