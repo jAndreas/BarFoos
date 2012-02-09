c865a36dbb98df15acfb62ef6777252953368e46
Andreas <jandy@typeofnan.com>
2012-02-09 14:30:55 +0100

    added jQueryUI access for .resizable()

--- production/barfoos-debug.js
+++ production/barfoos-debug.js
@@ -11964,6 +11964,11 @@ $.transform = {
 					
 					return this;
 				},
+				resizable:		function _resizable() {
+					$.fn.resizable.apply( this, arguments );
+					
+					return this;
+				},
 				slider:			function _slider() {
 					$.fn.slider.apply( this, arguments );
 					
